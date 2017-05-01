36d9763eb2f5376460bac25f9b218b8a1591869c
Albert Wang <a.wang@meteocontrol.com>
2017-05-01 11:21:32 +0800

    Add route

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
-import App from './app.vue'
+import routes from './routes'
 
 new Vue({
     el: '#app',
-    components: { App }
+    data: {
+        currentRoute: window.location.pathname
+    },
+    computed: {
+        ViewComponent() {
+            const matchingView = routes[this.currentRoute]
+            return matchingView ?
+                require('./pages/' + matchingView + '.vue') :
+                require('./pages/404.vue')
+        }
+    },
+    render(h) { return h(this.ViewComponent) }
+})
+
+window.addEventListener('popstate', () => {
+    app.currentRoute = window.location.pathname
 })
